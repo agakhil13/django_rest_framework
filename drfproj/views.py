d65fd3c3d16c18db2693f392c991dbf03b872e4c
agakhil13 <agakhil1310@gmail.com>
2022-04-28 11:13:40 +0530

    get post

--- drfproj/views.py
+++ drfproj/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import redirect, render
 from django.http import JsonResponse, HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from drfapp.serializers import StudentSerializer
+from drfapp.models import Student
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            'username' : 'admin',
-            'years_active' : 10
-        }
-        return Response(data)
+        qs = Student.objects.all()
+        serializer = StudentSerializer(qs, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
